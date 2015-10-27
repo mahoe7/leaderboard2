81fe5fa689bf87850bf5c4773ef62d4ed6ba78d2
Hexrus <mahoe7@hawaii.edu>
2015-10-27 10:10:52 -1000

    ICS314

--- leaderboard.js
+++ leaderboard.js
@@ -1,3 +1,27 @@
 console.log("Hello world");
 
+if(Meteor.isClient) {
+  Template.leaderboard.helpers({
+    'player': function() {
+      return PlayersList.find();
+    },
+    'selectedClass': function() {
+      return "selected";
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function() {
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+      var selectedPlayer = Session.get('selectedPlayer');
+      console.log(selectedPlayer);
+    }
+  });
+}
+
+if(Meteor.isServer) {
+  console.log("Hello server");
+}
+
 PlayersList = new Mongo.Collection('players');
