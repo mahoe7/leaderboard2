72990fd5be6ec7896607d8175b59bf69e226f03e
Hexrus <mahoe7@hawaii.edu>
2015-10-29 08:04:54 -1000

    ICS314

--- leaderboard.js
+++ leaderboard.js
@@ -1,9 +1,11 @@
 console.log("Hello world");
 
 if(Meteor.isClient) {
+  Meteor.subscribe('thePlayers');
+
   Template.leaderboard.helpers({
     'player': function() {
-      return PlayersList.find();
+      return PlayersList.find({}, {sort: {score: -1, name: 1}});
     },
     'selectedClass': function() {
       var playerId = this._id;
@@ -11,6 +13,10 @@ if(Meteor.isClient) {
       if(playerId == selectedPlayer) {
         return "selected";
       }
+    },
+    'showSelectedPlayer': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer);
     }
   });
 
@@ -20,12 +26,53 @@ if(Meteor.isClient) {
       Session.set('selectedPlayer', playerId);
       var selectedPlayer = Session.get('selectedPlayer');
       console.log(selectedPlayer);
+    },
+    'click .increment': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      Meteor.call('modifyPlayerScore', selectedPlayer, 5);
+    },
+    'click .decrement': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      Meteor.call('modifyPlayerScore', selectedPlayer, -5);
+    },
+    'click .remove': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      Meteor.call('removePlayerData', selectedPlayer);
     }
   });
+
+  Template.addPlayerForm.events({
+    'submit form': function(event) {
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      Meteor.call('insertPlayerData', playerNameVar);
+    }
+  })
 }
 
 if(Meteor.isServer) {
-  console.log("Hello server");
+  Meteor.publish('thePlayers', function() {
+    var currentUserId = this.userId;
+    return PlayersList.find({createdBy: currentUserId});
+  });
+  Meteor.methods({
+    'insertPlayerData': function(playerNameVar) {
+      var currentUserId = Meteor.userId();
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0,
+        createdBy: currentUserId
+      })
+    },
+    'removePlayerData': function(selectedPlayer) {
+      var currentUserId = Meteor.userId();
+      PlayersList.remove({_id: selectedPlayer, createdBy: currentUserId});
+    },
+    'modifyPlayerScore': function(selectedPlayer, scoreValue) {
+      var currentUserId = Meteor.userId();
+      PlayersList.update({_id: selectedPlayer, createdBy: currentUserId}, {$inc: {score: scoreValue}});
+    }
+  });
 }
 
 PlayersList = new Mongo.Collection('players');
